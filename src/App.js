3aaab0bfd11d20fd0a13c949881e7db777896c75
JoHo99 <ghdi926@naver.com>
2023-08-11 09:29:54 +0900

    props 기초

--- src/App.js
+++ src/App.js
@@ -3,10 +3,42 @@ import ExpenseItem from './components/ExpenseItem';
 
 const App = () => {
 
+  const expenses = [
+    {
+      title : "바나나",
+      price : 2000,
+      date : new Date(2023,3,23)
+    },
+    {
+      title : "BBQ",
+      price : 20000,
+      date : new Date(2023,5,21)
+    },
+    {
+      title : "도미노피자",
+      price : 35000,
+      date : new Date(2023,7,14)
+    }
+  ];
+
 
   return (
     <>
-      <ExpenseItem />
+      <ExpenseItem 
+      title={expenses[0].title} 
+      price = {expenses[0].price} 
+      date={expenses[0].date}
+      />
+      <ExpenseItem 
+      title={expenses[1].title} 
+      price = {expenses[1].price} 
+      date={expenses[1].date}
+      />
+      <ExpenseItem 
+      title={expenses[2].title} 
+      price = {expenses[2].price} 
+      date={expenses[2].date}
+      />
     </>
   );
 }
