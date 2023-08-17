a01ff98342825fbcd2c15d5f35ea374e66c59898
JoHo99 <ghdi926@naver.com>
2023-08-17 11:16:32 +0900

    목표설정 앱 추가기능 삭제기능

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
 import React, { useState } from 'react';
 import CourseInput from './components/CourseGoals/CourseInput';
 import CourseList from './components/CourseGoals/CourseList';
-import './App.css'
+import './App.css';
+
+
+const DUMMY_DATA = [
+  {
+    id: 'g1',
+    text: '리액트 컴포넌트 스타일링 마스터하기'
+  },
+  {
+    id: 'g2',
+    text: 'UI프로그래밍 고수되기'
+  },
+];
 
 const App = () => {
+
+  const [goals, setGoals] = useState(DUMMY_DATA);
+
+  // Input에게 전달할 함수
+  const addGoalHandler = text => {
+    console.log('전달받은 텍스트 : ', text);
+    const newGoal = {
+      id: Math.random().toString(),
+      text: text
+    };
+
+    // 상태 변수 수정
+    // const updateGoals = [...goals,newGoal]; setGoals(updateGoals);
+    setGoals(prevGoals => [...prevGoals, newGoal]);
+  };
+
+
+  // 삭제 이벤트 핸들러 courseitem까지 보내야함
+  const deleteGoalHandler = id => {
+    // // const updateGoals = [...goals];
+    // // const index = updateGoals.findIndex(goal => goal.id === id);
+    // const updateGoals = goals.filter(goal => goal.id !== id);
+    // setGoals(updateGoals);
+    setGoals((prevGoals) => prevGoals.filter(goal => goal.id !== id))
+  }
+
+
+  // courselist 조건부 렌더링
+  let listContent = <p style={{
+    color: 'red',
+    fontSize: '2em',
+    textAlign: 'center'
+  }}>목표를 등록해주세요!!</p>
+  if (goals.length > 0) {
+    listContent = <CourseList items={goals} onDelete={deleteGoalHandler} />;
+  }
+
+
   return (
     <div>
-      <section id='goal-form'>
-        <CourseInput />
+      <section id="goal-form">
+        <CourseInput onAdd={addGoalHandler} />
       </section>
-      <section id='goals'>
-        <CourseList />
+      <section id="goals">
+        {listContent}
       </section>
     </div>
   );
-}
-
-
+};
 
 export default App;
