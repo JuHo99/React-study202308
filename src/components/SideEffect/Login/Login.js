44a5017b91ed1b7f097a361f892c10dd6432b035
JoHo99 <ghdi926@naver.com>
2023-08-22 09:25:14 +0900

    useEffect 의존성 배열

--- src/components/SideEffect/Login/Login.js
+++ src/components/SideEffect/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../../UI/Card';
 import styles from './Login.module.css';
@@ -11,20 +11,21 @@ const Login = ({ onLogin }) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const emailChangeHandler = (e) => {
-    setEnteredEmail(e.target.value);
-
+  // 입력란을 모두 체크하여 from의 버튼disabled를 해제하는 상태변수
+  // fomIsValid의 사이드 이펙트를 처리하는 영역
+  useEffect(()=>{
+    console.log(`useEffect call in Login.js`);
     setFormIsValid(
-      e.target.value.includes('@') && enteredPassword.trim().length > 6
+      enteredEmail.includes('@') && enteredPassword.trim().length > 6
     );
+  },[enteredEmail,enteredPassword]);//의존성 배열에 상태변수를 넣어주면 상태변수가 set으로 바뀔 때마다 useEffect를 재실행
+
+  const emailChangeHandler = (e) => {
+    setEnteredEmail(e.target.value);  
   };
 
   const passwordChangeHandler = (e) => {
     setEnteredPassword(e.target.value);
-
-    setFormIsValid(
-      e.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
