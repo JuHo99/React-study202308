2c949d907ddda5fa641ef0ddbb9576f45d7bb927
JoHo99 <ghdi926@naver.com>
2023-08-23 09:31:57 +0900

    Context API 기초

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import React, { useEffect, useState } from 'react';
 import MainHeader from './components/SideEffect/MainHeader/MainHeader';
 import Home from './components/SideEffect/Home/Home';
 import Login from './components/SideEffect/Login/Login';
+import AuthContext from './store/auth-context';
 
 const App = () => {
 
   // 로그인 상태를 관리하는 변수
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // 화면이 리렌더링될 때 localStorage를 확인해서
-  // 현재 로그인 플래그가 존재하는지 검사
-  // console.log('로그인 검사 수행!');
-  // const storedLoginFlag = localStorage.getItem('login-flag');
-  // if (storedLoginFlag === '1') {
-  //   setIsLoggedIn(true);
-  // }
 
   // 기존의 로그인 한 사람인지 확인하는 코드 (최초 한번만 실행)
   useEffect(() => {
@@ -40,13 +33,16 @@ const App = () => {
 
 
   return (
-    <>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn
+    }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {isLoggedIn && <Home />}
         {!isLoggedIn && <Login onLogin={loginHandler} />}
       </main>
-    </>
+    </AuthContext.Provider>
+
   );
 };
 
